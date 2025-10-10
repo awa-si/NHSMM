349dd1c2b7309fa78795a62ef7d470b994121766
awwea <admin@awwea.com>
2025-10-10 17:32:13 +0200

    introducing NeuralTransition

--- nhsmm/distributions/__init__.py
+++ nhsmm/distributions/__init__.py
@@ -1,10 +1,12 @@
-from .NeuralDuration import NeuralDuration
 from .NeuralGaussian import NeuralGaussian
 from .NeuralMultinomial import NeuralMultinomial
+from .NeuralDuration import NeuralDuration
+from .NeuralTransition import NeuralTransition
 
 
 __all__ = [
     'NeuralDuration',
+    'NeuralTransition',
     'NeuralGaussian',
     'NeuralMultinomial',
 ]
